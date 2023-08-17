d885977392e7545a94791103913da74928e00da0
Myakinina Yulia <myakinina_yu@mail.ru>
2023-08-17 16:27:51 +0300

    Add getting product data from server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/models/product';
-import { products  as productsData} from './data/products'
+import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-clothes-shop';
 
-  products: IProduct[] = productsData;
+  products!: IProduct[];
+
+  constructor(private productsService: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe( products => { this.products = products });
+  }
 }
