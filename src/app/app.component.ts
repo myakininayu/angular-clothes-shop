5b0acd98fba2b890532f52eaf0d62e29aa29eaa7
Myakinina Yulia <myakinina_yu@mail.ru>
2023-08-18 12:13:38 +0300

    Add product filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
   title = 'angular-clothes-shop';
   loading = false;
   products$!: Observable<IProduct[]>;
+  term = '';
 
   constructor(private productsService: ProductsService) { }
 
